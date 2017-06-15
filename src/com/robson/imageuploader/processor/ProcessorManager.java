cc942e1f093549cc6c592525d4a9f3000851704a
Rocky Robson <RockyRobson@hotmail.com>
2017-06-15 16:13:03 -0700

    Refactor

--- src/com/robson/imageuploader/processor/ProcessorManager.java
+++ src/com/robson/imageuploader/processor/ProcessorManager.java
@@ -13,16 +13,27 @@ public class ProcessorManager {
 	private ImageParser imgParser;
 	private ImageProcessor imgProcessor;
 
+	/**
+	 * Create a ProcessorManager. Prepare image from url
+	 */
 	public ProcessorManager(ProcessorProperties props, String url) throws MalformedURLException {
 		imgParser = new ImageParser(props.getPixelSize());
 		imgProcessor = new ImageProcessor(imgParser.parse(url), props);
 	}
 
+	/**
+	 * Create a ProcessorManager. Prepare image from a file
+	 */
 	public ProcessorManager(ProcessorProperties props, File file) throws InvalidFormatException {
 		imgParser = new ImageParser(props.getPixelSize());
 		imgProcessor = new ImageProcessor(imgParser.parse(file), props);
 	}
 
+	/**
+	 * Process an image
+	 * 
+	 * @return An image as a string
+	 */
 	public String getImage() {
 		return imgProcessor.process();
 	}
